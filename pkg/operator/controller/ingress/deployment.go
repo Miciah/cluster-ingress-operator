4176c235159fd745f511918a59b63a2e4107a1fc
Miciah Masters <miciah.masters@gmail.com>
2021-04-30 16:18:58 -0400

    Allow TLSv1.3 and the "Modern" TLS profile
    OpenShift 4.6 is built using UBI 8, which has OpenSSL 1.1.1, which supports
    TLSv1.3, so allow specifying spec.tlsSecurityProfile.type: Modern or
    spec.tlsSecurityProfile.custom.minTLSVersion: VersionTLS13.
    
    * pkg/operator/controller/ingress/controller.go
    tlsProfileSpecForSecurityProfile): Allow use of the "Modern" profile.
    (validTLSVersions): Add configv1.VersionTLS13.
    (validateTLSSecurityProfile): Verify that some non-TLSv1.3 cipher is
    specified if minTLSVersion is a version below TLSv1.3, and verify that some
    TLSv1.3 cipher is specified if minTLSVersion is TLSv1.3.
    * pkg/operator/controller/ingress/controller_test.go
    (TestTLSProfileSpecForSecurityProfile)
    (TestTLSProfileSpecForIngressController): Verify that the "Modern" profile
    is used if it is specified.
    * pkg/operator/controller/ingress/deployment.go (desiredRouterDeployment):
    Allow TLSv1.3.  Set ROUTER_CIPHERS based on the non-TLSv1.3 ciphers in the
    ingresscontroller's TLS profile, and set ROUTER_CIPHERSUITES based on the
    TLSv1.3 ciphers.
    (inferTLSProfileSpecFromDeployment): Allow TLSv1.3.
    * pkg/operator/controller/ingress/deployment_test.go
    (TestDesiredRouterDeployment): Verify that SSL_MIN_VERSION is set to
    TLSv1.3 if TLSv1.3 was specified on the ingresscontroller.  Verify that
    ROUTER_CIPHERSUITES is set if the TLS profile specifies any TLSv1.3
    ciphers, and verify that ROUTER_CIPHERSUITES has TLSv1.3 ciphers and
    ROUTER_CIPHERS has other ciphers.
    (TestInferTLSProfileSpecFromDeployment): Add test cases for
    SSL_MIN_VERSION=TLSv1.2 and for SSL_MIN_VERSION=TLSv1.3.

--- pkg/operator/controller/ingress/deployment.go
+++ pkg/operator/controller/ingress/deployment.go
@@ -581,8 +581,24 @@ func desiredRouterDeployment(ci *operatorv1.IngressController, ingressController
 
 	tlsProfileSpec := tlsProfileSpecForIngressController(ci, apiConfig)
 
-	ciphers := strings.Join(tlsProfileSpec.Ciphers, ":")
-	env = append(env, corev1.EnvVar{Name: "ROUTER_CIPHERS", Value: ciphers})
+	var tls13Ciphers, otherCiphers []string
+	for _, cipher := range tlsProfileSpec.Ciphers {
+		if tlsVersion13Ciphers.Has(cipher) {
+			tls13Ciphers = append(tls13Ciphers, cipher)
+		} else {
+			otherCiphers = append(otherCiphers, cipher)
+		}
+	}
+	env = append(env, corev1.EnvVar{
+		Name:  "ROUTER_CIPHERS",
+		Value: strings.Join(otherCiphers, ":"),
+	})
+	if len(tls13Ciphers) != 0 {
+		env = append(env, corev1.EnvVar{
+			Name:  "ROUTER_CIPHERSUITES",
+			Value: strings.Join(tls13Ciphers, ":"),
+		})
+	}
 
 	var minTLSVersion string
 	switch tlsProfileSpec.MinTLSVersion {
@@ -593,8 +609,8 @@ func desiredRouterDeployment(ci *operatorv1.IngressController, ingressController
 		minTLSVersion = "TLSv1.1"
 	case configv1.VersionTLS12:
 		minTLSVersion = "TLSv1.2"
-	// TODO: Add TLS 1.3 support when haproxy is built with an openssl
-	//  version that supports tls v1.3.
+	case configv1.VersionTLS13:
+		minTLSVersion = "TLSv1.3"
 	default:
 		minTLSVersion = "TLSv1.2"
 	}
@@ -815,7 +831,8 @@ func inferTLSProfileSpecFromDeployment(deployment *appsv1.Deployment) *configv1.
 		minTLSVersion = configv1.VersionTLS11
 	case "TLSv1.2":
 		minTLSVersion = configv1.VersionTLS12
-	// TODO: Add TLS 1.3 support when haproxy is built with openssl 1.1.1.
+	case "TLSv1.3":
+		minTLSVersion = configv1.VersionTLS13
 	default:
 		minTLSVersion = configv1.VersionTLS12
 	}
